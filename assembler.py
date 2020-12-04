1804bdb65a2eb53bfc0c681a21b27c4a593383fa
prashanth <prashanth.flash98@gmail.com>
2020-12-03 18:56:55 -0600

    added sequence reading

--- assembler.py
+++ assembler.py
@@ -1,15 +1,27 @@
+import os
 import argparse
+from Bio import SeqIO
 
 
 def read_sequences(directory):
+
+    files = os.listdir(directory)
+    sequences = []
+
+    for file in files:
+        sequence = ""
+        filename = os.path.join(directory, file)
+        for record in SeqIO.parse(filename, "fastq"):
+            sequence = sequence + ''.join(list(record.seq))
+        sequences.append(sequence)
+    return sequences
+
+
+def split_into_kmers(sequence, k):
     return []
 
 
-def split_into_kmers(sequence, args):
-    return []
-
-
-def generate_de_bruijin_graph(kmers, args):
+def generate_de_bruijin_graph(kmers, k1mers, args):
     return []
 
 
@@ -26,7 +38,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--k", default=30, help="k-mer size")
     parser.add_argument("--threshold", default=2, help="min count of k-mers to consider")
-    parser.add_argument("--input", default="sars-cov-2-raw-data", help="input directory")
+    parser.add_argument("--input", default="sars-cov-2-trimmed/SE-SW-4-15", help="input directory")
     parser.add_argument("--reference", default=None, help="reference genome")
 
     args = parser.parse_args()
@@ -34,8 +46,9 @@ if __name__ == "__main__":
     # each file is a seperate sequence
     sequences = read_sequences(args.input)
 
-    for sequence in sequences:
-        kmers = split_into_kmers(sequence, args)
-        graph = generate_de_bruijin_graph(kmers, args)  # save the graph for plotting
-        assembled_seq = traverse_graph(graph)
-        mismatches = align_to_reference(assembled_seq, args)  # save the mismatches for plotting
+    # for sequence in sequences:
+    #     kmers = split_into_kmers(sequence, args.k)
+    #     k1mers = split_into_kmers(sequence, args.k - 1)
+    #     graph = generate_de_bruijin_graph(kmers, k1mers, args)  # save the graph for plotting
+    #     assembled_seq = traverse_graph(graph)
+    #     mismatches = align_to_reference(assembled_seq, args)  # save the mismatches for plotting
