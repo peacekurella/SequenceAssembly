ccc946d628331aac9951f22614aee76093008749
Tanveer Singh Virdi <virdi004@umn.edu>
2020-12-05 22:04:36 -0600

    Implemented De Bruijn Graph Creation
    Signed-off-by: Tanveer Singh Virdi <virdi004@umn.edu>

--- assembler.py
+++ assembler.py
@@ -22,7 +22,6 @@ def read_sequences(directory):
 
 
 def split_into_kmers(sequence, k, threshold):
-
     kmers = {}
 
     for i in range(0, len(sequence), k):
@@ -40,8 +39,49 @@ def split_into_kmers(sequence, k, threshold):
     return kmers
 
 
-def generate_de_bruijin_graph(kmers, k1mers, args):
-    return []
+def generate_de_bruijin_graph(kmers):
+    # Set of all nodes in the DB Graph
+    nodes = set()
+    # Set of nodes having in-degrees
+    not_starts = set()
+    # List of all directed edges in the graph
+    edges = []
+
+    for kmer in kmers:
+        # From k-mers, get k-1mers
+        k1mer1 = kmer[:-1]
+        k1mer2 = kmer[1:]
+        # Add k-1 mers to the nodes list
+        nodes.add(k1mer1)
+        nodes.add(k1mer2)
+
+        # Add an edge between the two k-1mers
+        edges.append((k1mer1, k1mer2))
+
+        # Add destination node to the set of nodes having in degrees
+        not_starts.add(k1mer2)
+
+    start_nodes = list(nodes - not_starts)
+
+    # Return the nodes, edges and the starting nodes in the graph.
+    return nodes, edges, start_nodes
+
+
+def make_node_edge_map(edges):
+
+    # Make a map of starting nodes to the adjacency list of that node
+    node_edge_map = {}
+
+    # Go through all edges
+    for e in edges:
+        n = e[0]
+        # If start node exists, add destination node to adjacency list
+        if n in node_edge_map:
+            node_edge_map[n].append(e[1])
+        # Add start node to map and initialize the adjacency list with the destination node
+        else:
+            node_edge_map[n] = [e[1]]
+    return node_edge_map
 
 
 def traverse_graph(graph):
